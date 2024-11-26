5b7753258c6604d97e7c308e50c54fe56e03066a
rien-isaac <rcjurado@sscgi.com>
2024-11-26 17:25:52 +0800

    Added table for history

--- activity-02/index.js
+++ activity-02/index.js
@@ -1,7 +1,11 @@
 let currentBalance = 50;
-var history = document.getElementById("history-logs");
+// var textBalance = document.getElementById(txtBalance);
+
+// textBalance.textContent += currentBalance;
 
 function transferLoad() {
+  var history = document.getElementById("history-logs");
+
   let mobileNumber = document.getElementById("mobile-number").value;
   let amount = document.getElementById("amount").value;
 
@@ -12,15 +16,17 @@ function transferLoad() {
     loanBalance();
   } else {
     currentBalance = currentBalance - amount;
+    history.textContent += "-" + amount;
     console.log("-" + amount);
     console.log(currentBalance);
   }
-}
 
-function loanBalance() {
-  let loanAmount = 500;
-  currentBalance = currentBalance + loanAmount;
-  console.log("umutang ka ng " + loanAmount);
-  console.log("+ " + loanAmount);
-  console.log(currentBalance);
+  function loanBalance() {
+    let loanAmount = 500;
+    currentBalance = currentBalance + loanAmount;
+    history.textContent += " umutang ka ng " + loanAmount;
+    history.textContent += "+" + loanAmount;
+    console.log("+ " + loanAmount);
+    console.log(currentBalance);
+  }
 }
