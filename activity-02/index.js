aa56e424d6bf79f5f3aae9dff186d73e795fabee
rien-isaac <rcjurado@sscgi.com>
2024-11-27 13:16:07 +0800

    Add validation

--- activity-02/index.js
+++ activity-02/index.js
@@ -2,24 +2,57 @@ let accountNumber;
 
 let loginBtn = document.getElementById("loginBtn");
 
+let accounts = [
+  {
+    mobile: "11111111111",
+    password: "testpass",
+    balance: 300,
+  },
+  {
+    mobile: "00000000000",
+    password: "passtest",
+    balance: 50,
+  },
+];
+
 function login() {
   accountNumber = document.getElementById("account").value;
+  password = document.getElementById("password").value;
   let splitAccNum = accountNumber.split("");
   let mobileNumberLength = splitAccNum.length;
 
   //   console.log(mobileNumberLength);
 
   if (mobileNumberLength != 11) {
-    alert("Invalid Number!");
-    document.getElementById("account").value = "";
+    invalidNumber("invalid");
   } else {
-    //   makalogin ka dapat rito
-    alert("Welcome " + accountNumber);
-    window.location.href = "./load.html";
-    // console.log("yehey");
+    // check muna sino nag login
+    checkAccount();
   }
 }
 
 function checkAccount() {
+  // console.log("You made it here");
   // validation here
+  for (let checker = 0; checker < accounts.length; checker++) {
+    if (
+      accountNumber == accounts[checker].mobile &&
+      password == accounts[checker].password
+    ) {
+      //   makalogin ka dapat rito
+      alert("Welcome " + accountNumber);
+      window.location.href = "./load.html";
+    }
+  }
+}
+
+function invalidNumber(type) {
+  if (type == "invalid") {
+    alert("Invalid Number!");
+  } else if (type == "no record") {
+    alert("Incorrect Number or Password");
+  }
+
+  document.getElementById("account").value = "";
+  document.getElementById("password").value = "";
 }
